6e4c97d9ea1fc8dc35f9a928ce17559de8a95431
simens <397389173@qq.com>
2020-06-05 15:37:19 +0800

    modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = { // 在这里设置路径是因为发布到github pages上的时候，url存在子路径
-  publicPath: process.env.NODE_ENV === 'production' ? '/circledemo' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/travis-CI-demo' : '/'
 }
