94f4f0ce5ba3b3cd29f81ef3b34277dbab4e80c4
vs19861127 <397389173@qq.com>
2020-06-30 19:53:35 +0800

    change tab name

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,11 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">关于</router-link>
     </div>
     <router-view/>
   </div>
 </template>
-
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
